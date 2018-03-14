d6620cbe04a48296e0c23da73ccd60eb35ac27ea
akritrime <akritrime@gmail.com>
2018-03-14 19:54:49 +0530

    minimal kernel

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_std]
 #![no_main]
 
+extern crate rlibc;
+
 #[lang = "panic_fmt"]
 #[no_mangle]
 
@@ -11,8 +13,19 @@ pub extern fn rust_begin_panic(_msg: core::fmt::Arguments, _file: &'static str, 
     }
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *const u8 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xa;
+        }
+    }
+    
     loop {
 
     }
